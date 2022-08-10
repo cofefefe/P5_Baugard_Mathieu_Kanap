f34107f018c61573ddf878b6df87597d86bc4931
Mathieu Baugard <mathieu.baugard@g-mail.fr>
2022-08-10 03:32:35 +0200

    feat:error msg quantity/color selected are not valid

--- js/product.js
+++ js/product.js
@@ -40,6 +40,48 @@ fetch('http://localhost:3000/api/products/' + productId)
 
 let addToCartButton = document.getElementById("addToCart")
 
+// Add the Id, quantity and color selected by client to the object " productToAddInLocalStorage "
+function addProductParam() {
+addToCartButton.addEventListener("click", () => {
+
+    let quantitySelected = parseInt(document.getElementById("quantity").value);
+    let colorSelected = document.getElementById('colors').value
+    // define client's params
+    let productToAddInLocalStorage =
+        {
+            id: productId,
+            quantity: quantitySelected,
+            color: colorSelected
+        }
+
+    // On contrôle les données saisies
+    if (areFormDataValid(productToAddInLocalStorage) === false) {
+        return;
+    }
+
+    // add product to the local storage with their new params
+    addProductInLocalStorage(productToAddInLocalStorage);
+
+    // Redirect to the cart page after adding an item to ls
+    document.location.href = "cart.html";
+})
+}
+addProductParam()
+
+function areFormDataValid(productToAddInLocalStorage) {
+    if (productToAddInLocalStorage.color === '') {
+        alert('Mauvaise couleur');
+        return false;
+    }
+
+    if (!productToAddInLocalStorage.quantity > 0) {
+        alert('Mauvaise quantité');
+        return false;
+    }
+
+    return true;
+}
+
 // make an array of the local storage, empty array if LS is empty, an object " products " is push if there's something in ls
 function getProductsFromLocalStorage() {
     let products = localStorage.getItem("products")
@@ -48,6 +90,25 @@ function getProductsFromLocalStorage() {
     } else {
         return JSON.parse(products);
     }
+}
+
+// Add product selected by client to the Local Storage
+function addProductInLocalStorage(productToAddInLocalStorage) {
+    // take products already in LS
+    let productsFromLocalStorage = getProductsFromLocalStorage();
+
+    // May this product is already existing in ls ?
+    let productKeyInLocalStorage = findProductKeyInLocalStorage(productToAddInLocalStorage, productsFromLocalStorage);
+
+    // Update " products " with the new item, or adapt quantity if he's already existing in LS
+    if (productKeyInLocalStorage === null) {
+        productsFromLocalStorage.push(productToAddInLocalStorage);
+    } else {
+        productsFromLocalStorage[productKeyInLocalStorage].quantity += productToAddInLocalStorage.quantity;
+    }
+
+    // Updating LS
+    localStorage.setItem('products', JSON.stringify(productsFromLocalStorage));
 }
 // Search and compare display Product with product in local Storage
 function findProductKeyInLocalStorage(productToAddInLocalStorage, products) {
@@ -60,47 +121,3 @@ function findProductKeyInLocalStorage(productToAddInLocalStorage, products) {
     });
     return productKeyFound;
 }
-
-// Add product selected by client to the Local Storage
-function addProductInLocalStorage(productToAddInLocalStorage) {
-    // take products already in LS
-    let products = getProductsFromLocalStorage();
-
-    // May this product is already existing in ls ?
-    let productKeyInLocalStorage = findProductKeyInLocalStorage(productToAddInLocalStorage, products);
-
-    // Update " products " with the new item, or adapt quantity if he's already existing in LS
-    if (productKeyInLocalStorage === null) {
-        products.push(productToAddInLocalStorage);
-    } else {
-        let productToUpdate = products[productKeyInLocalStorage];
-        productToUpdate.quantity += productToAddInLocalStorage.quantity;
-        products[productKeyInLocalStorage] = productToUpdate;
-    }
-
-    // Updating LS
-    localStorage.setItem('products', JSON.stringify(products));
-}
-    // Add the Id, quantity and color selected by client to the object " productToAddInLocalStorage "
-function addProductParam() {
-    addToCartButton.addEventListener("click", () => {
-
-    let quantitySelected = parseInt(document.getElementById("quantity").value);
-    let colorSelected = document.getElementById('colors').value
-    // define client's params
-    let productToAddInLocalStorage =
-        {
-        id: productId,
-        quantity: quantitySelected,
-        color: colorSelected
-        }
-
-    // add product to the local storage with their new params
-    addProductInLocalStorage(productToAddInLocalStorage);
-
-    // Redirect to the cart page after adding an item to ls
-    document.location.href = "cart.html";
-    })
-}
-addProductParam()
-    
